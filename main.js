1b8dc7316c7129dad40e2fbe850ed5542436216b
FadilMS18 <muhfadilms2418@gmail.com>
2024-10-27 17:44:47 +0800

    Added the first 3 Task

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 console.log('Hello, world!')
+
+function show(){
+    return 'CONSOLE'
+}
+
+function capitalize(string){
+    let capital = string.charAt(0).toUpperCase()
+    return capital + string.slice(1)
+}
+
+
+function reverse(string){
+    let reversed = string.split('').reverse()
+    return reversed.join('')
+}
+
+const calculator = {
+    add: (a, b) => a + b,
+    sub: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => parseInt(a / b)
+}
+
+console.log(10 / 3)
+
+export {show, capitalize, reverse, calculator}
